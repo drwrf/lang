68885ec562d2939d9a187ef0233f10a519514de5
♥ <hi@drwrf.com>
2015-12-22 23:44:04 -0500

    Clean up shared example.

--- spec/token/shared_examples.rb
+++ spec/token/shared_examples.rb
@@ -1,7 +1,21 @@
-shared_examples_for "a token" do |examples|
+# Affords shared examples for token parsing.
+#
+# The second argument is a hash where the keys are text
+# to tokenize, and the values are the expected lexeme.
+# Naturally this assumes that the text tokenizes into
+# only one token.
+#
+# example:
+#
+# describe Lang::Token::Number do
+#   shared_examples_for "a tokenizer", {
+#     "1" => "1"
+#   }
+# end
+shared_examples_for "a tokenizer" do |examples|
   examples.each do |input, lexeme|
     describe "#consume" do
-      it "consumes #{input}" do
+      it "tokenizes #{input}" do
         token = tokenize(input).next
 
         expect(token).to be_a(described_class)
--- spec/token/string_spec.rb
+++ spec/token/string_spec.rb
@@ -1,5 +1,5 @@
 describe Lang::Token::String do
-  it_behaves_like "a token", {
+  it_behaves_like "a tokenizer", {
     "'single-quotes'" => "single-quotes",
     '"double-quotes"' => "double-quotes",
     "'new\nlines'"    => "new\nlines",
